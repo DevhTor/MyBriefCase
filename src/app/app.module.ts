6539e67a842509f61427cef78f063b26078dce6c
hectoristy <34715623+hectoristy@users.noreply.github.com>
2023-08-10 21:04:18 -0400

    componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlueButtonComponent } from './blue-button/blue-button.component';
 import { BlueButton2Component } from './blue-button2/blue-button2.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { WellcomeComponent } from './wellcome/wellcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlueButtonComponent,
-    BlueButton2Component
+    BlueButton2Component,
+    NavBarComponent,
+    WellcomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/blue-button/blue-button.component.ts
+++ src/app/blue-button/blue-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-blue-button',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./blue-button.component.css']
 })
 export class BlueButtonComponent {
-  buttonName = 'Blue Button';
+  @Input() buttonName: string = 'Button';
 }
