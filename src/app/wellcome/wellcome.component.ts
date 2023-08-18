a9b2191c172903efe1c9d858ed323eb6b1249f2c
hectoristy <34715623+hectoristy@users.noreply.github.com>
2023-08-17 22:09:56 -0400

    router link added

--- src/app/wellcome/wellcome.component.ts
+++ src/app/wellcome/wellcome.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wellcome',
   templateUrl: './wellcome.component.html',
   styleUrls: ['./wellcome.component.css']
 })
+
+
+
 export class WellcomeComponent {
 
+  constructor(private router: Router) {}
+  
+  redirectToProjects() {
+    this.router.navigate(['Projects']);
+  }
 }
