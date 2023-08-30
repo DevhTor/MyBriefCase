cabcbd9b564fc8481bfb1dd52189fb135beab867
hectoristy <34715623+hectoristy@users.noreply.github.com>
2023-08-29 20:39:33 -0400

    scroll background change added

--- src/app/nav-bar-shell/nav-bar-shell.component.ts
+++ src/app/nav-bar-shell/nav-bar-shell.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar-shell',
@@ -7,8 +7,16 @@ import { Component } from '@angular/core';
 })
 export class NavBarShellComponent {
   menuVisible: boolean = false;
+  isScrolled: boolean = false;
+
   
   toggleMenu() {
     this.menuVisible = !this.menuVisible;
+    
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
   }
 }
